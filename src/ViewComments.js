8083e2c8c2031458c0d3fa1704b49e72ddc9b6df
Poornesh <poornesh9626@gmail.com>
2022-02-04 20:02:33 +0530

    tried working with CORS

--- src/ViewComments.js
+++ src/ViewComments.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ViewComments = ({ postID }) => {
-  const baseUrl = 'https://jsonplaceholder.typicode.com';
+  const baseUrl = "https://jsonplaceholder.typicode.com";
   const [comments, SetComments] = useState([]);
   const [commentsLoading, SetCommentsLoading] = useState(false);
   useEffect(async () => {
@@ -15,12 +15,32 @@ const ViewComments = ({ postID }) => {
       SetCommentsLoading(false);
     }
   }, [postID]);
+
+  useEffect(() => {
+    fetch("https://cors-demo.glitch.me/", {
+      menthod: "GET",
+      mode : 'no-cors' //add this line to bypass the CORS error
+    })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+
+      // axios({
+      //   menthod:'GET',
+      //   url:'https://cors-demo.glitch.me/',
+      //   headers: {
+      //     'Access-Control-Allow-Origin': '*',
+      //     'Content-Type': 'application/json',
+      //   }
+      // }).then(res=>{
+      //   console.log(res, 'in axios')
+      // }).catch(error => console.log(error, 'in axios'))  //not working
+  }, []);
   return (
     <div>
       {commentsLoading
-        ? 'Loading Comments'
+        ? "Loading Comments"
         : comments.map((item, index) => (
-            <small key={item.id}>{index + 1 + '. ' + item.body}</small>
+            <small key={item.id}>{index + 1 + ". " + item.body}</small>
           ))}
     </div>
   );
